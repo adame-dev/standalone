271f8129c5ac7b915ee89bb1b0157d3fdf4579d6
NickBlade <javiadamrius@gmail.com>
2022-04-20 15:03:36 +0200

    Upload

---
+++ create_script.bat
@@ -0,0 +1,39 @@
+@echo off
+set /p scriptname="Script name: "
+set /p scriptdescription="Script description: "
+mkdir %scriptname%
+echo fx_version 'adamant' > %scriptname%/fxmanifest.lua
+echo game 'gta5' >> %scriptname%/fxmanifest.lua
+echo description '%scriptdescription%' >> %scriptname%/fxmanifest.lua
+echo version '1.0.0' >> %scriptname%/fxmanifest.lua
+echo server_scripts { >> %scriptname%/fxmanifest.lua
+echo 	'config.lua', >> %scriptname%/fxmanifest.lua
+echo 	'server/main.lua' >> %scriptname%/fxmanifest.lua
+echo } >> %scriptname%/fxmanifest.lua
+echo client_scripts { >> %scriptname%/fxmanifest.lua
+echo 	'config.lua', >> %scriptname%/fxmanifest.lua
+echo 	'client/main.lua' >> %scriptname%/fxmanifest.lua
+echo } >> %scriptname%/fxmanifest.lua
+
+echo Config = {} > %scriptname%/config.lua
+
+cd %scriptname%
+mkdir client
+mkdir server
+
+cd client
+copy NUL client.lua
+cd ../server
+copy NUL server.lua
+
+
+
+
+
+
+
+
+
+
+
+
